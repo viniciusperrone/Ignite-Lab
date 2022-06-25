4c38f0e88696960064e293b19de0e45159b68789
Vinicius Perrone <perronevinicius2018@gmail.com>
2022-06-25 16:44:31 -0300

    chore: Subscriber page completed

--- event-platform/src/lib/apollo.ts
+++ event-platform/src/lib/apollo.ts
@@ -1,6 +1,9 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client';
 
 export const client = new ApolloClient({
-  uri: 'https://api-sa-east-1.graphcms.com/v2/cl4otc0wy0w6701xt5tow3l9z/master',
+  uri: import.meta.env.VITE_API_URL,
+  headers: {
+    'Authorization': `Bearer ${import.meta.env.VITE_API_ACCESS_TOKEN}`
+  },
   cache: new InMemoryCache()
 })
